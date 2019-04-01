b8ec57917a4b6951909ff81cf94674eac626699e
Carl Durand <carldurand.student@careerdevs.com>
2019-04-01 15:11:58 +0000

    lesson13

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
 // delete books
 list.addEventListener('click', (e) => {
@@ -6,4 +7,29 @@ list.addEventListener('click', (e) => {
     const li = e.target.parentElement;
     li.parentNode.removeChild(li);
   }
+});
+
+// add books
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
